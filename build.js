366eb301e197648257e8d6ed9352b4f4d0c1b904
Evandro Junior <evandrojr@gmail.com>
2017-08-08 18:37:42 -0300

    corrigido bug sumario

--- build.js
+++ build.js
@@ -38,24 +38,53 @@ const cheerio = require('cheerio')
 const $ = cheerio.load(contents, 'utf8');
 
 $('h1').each(function(i, elem) {
-    $(this).text((i  +  1) + " " + $(this).text());
-    $(this).data('idx', i  +  1);
+    let idx = i+1;
+    $(this).text(idx + " " + $(this).text());
+    $(this).data('idx', idx);
     $(this).addClass('summary');
 });
 
+let subIdx = 1;
+let lastIdx = 0;
 $('h2').each(function(i, elem) {
     let anterior = $(this).prevAll('h1');
     let idx = anterior.data('idx');
-    $(this).data('idx', idx + '.' + (i + 1));
-    $(this).text(idx + '.' + (i + 1) + ' ' + $(this).text());
+    if(lastIdx==0){
+        lastIdx = idx;
+    }else{
+        if(lastIdx == idx){
+            subIdx++;
+        }else{
+            subIdx = 1;
+        }
+        lastIdx = idx;
+    }
+    console.log(idx, subIdx, lastIdx);
+    $(this).data('idx', idx + '.' + subIdx);
+    $(this).text(idx + '.' + subIdx + ' ' + $(this).text());
     $(this).addClass('summary');
 });
 
+// return;
+
+subIdx = 1;
+lastIdx = 0;
 $('h3').each(function(i, elem) {
     let anterior = $(this).prevAll('h2');
     let idx = anterior.data('idx');
-    $(this).data('idx', idx + '.' + (i + 1));
-    $(this).text(idx + '.' + (i + 1) + ' ' + $(this).text());
+    if(lastIdx==0){
+        lastIdx = idx;
+    }else{
+        if(lastIdx == idx){
+            subIdx++;
+        }else{
+            subIdx = 1;
+        }
+        lastIdx = idx;
+    }
+    console.log(idx, subIdx, lastIdx);
+    $(this).data('idx', idx + '.' + subIdx);
+    $(this).text(idx + '.' + subIdx + ' ' + $(this).text());
     $(this).addClass('summary');
 });
 
